b4fe42f6c43a892e7924747162063c3ef6ca7168
Bruno Ferreira Silva <33335907+brunoe2a@users.noreply.github.com>
2026-03-28 16:22:10 -0400

    Bug s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'public'),
+    'default' => strtolower(env('FILESYSTEM_DISK', 'public')),
 
     /*
     |--------------------------------------------------------------------------
